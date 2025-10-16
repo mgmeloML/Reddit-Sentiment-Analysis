0e716ceae054a3b6fdfa3788ea71f6d0cee3b10d
Mayowa Ogundele (k23004722) <profmayowa@gmail.com>
2025-10-16 14:42:18 +0100

    Refactor utility functions for sentiment analysis: improve date handling and add example display function

--- utlity.py
+++ utlity.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 
 def subreddit_list(df):
   return pd.unique(df["Subreddit"])
@@ -14,10 +15,11 @@ def valid_time_range(df):
 def subreddit_range(df, subreddits):
   df = df.loc[df["Subreddit"].isin(subreddits)]
   return df
-
-
+  
 def time_range(df, start_date, end_date):
-  df['Post Time'] = pd.to_datetime(df['Post Time'])
+  df['Post Time'] = pd.to_datetime(df['Post Time'], errors='coerce')
+  start_date = datetime.combine(start_date, datetime.min.time())
+  end_date = datetime.combine(end_date, datetime.max.time())
   df_new = (df["Post Time"] > start_date) & (df["Post Time"] < end_date)
   return df.loc[df_new]
 
@@ -67,3 +69,13 @@ def sentiment_line_chart(df, subreddits, start_date, end_date):
   df = time_range(df, start_date, end_date)
   df = df.groupby(["Sentiment", "Post Time"]).size().reset_index(name="count")
   return px.line(df, x="Post Time", y="count", color="Sentiment", markers=True, line_shape="spline")
+
+def show_examples(df, subreddits, start_date, end_date):
+  df = subreddit_range(df, subreddits)
+  df = time_range(df, start_date, end_date)
+  df.drop(axis=1,columns="Title + Text")
+  df["Sentiment"] = df["Sentiment"].str.lower()
+  df_pos = df[df["Sentiment"] == "positive"]
+  df_neu = df[df["Sentiment"] == "neutral"]
+  df_neg = df[df["Sentiment"] == "negative"]
+  return df_pos, df_neu, df_neg
